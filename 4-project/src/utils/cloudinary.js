9f488567e56c1a70ddb6c6b0f8e333364d581809
tanmay-exists <tanmayrewale232021@gmail.com>
2025-01-30 19:19:34 +0530

    cloudinary util

--- 4-project/src/utils/cloudinary.js
+++ 4-project/src/utils/cloudinary.js
@@ -2,8 +2,26 @@ import { v2 as cloudinary } from 'cloudinary'
 import fs from 'fs'
 
 // Configuration
-    cloudinary.config({ 
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-        api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
-    });
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if(!localFilePath) return null
+    // upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: 'auto'
+    })
+    console.log("File is uploaded on cloudinary", response.url)
+    return response
+  } catch (error) {
+    fs.unlinkSync(localFilePath) // removes the locally saved temporary file as the upload operation got failed
+    return null
+  }
+}
+
+export { uploadOnCloudinary }
