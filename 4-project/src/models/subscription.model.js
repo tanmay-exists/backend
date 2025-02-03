fd984c00739cc7f1a8e91f03b7e1772e6ca37e18
tanmay-exists <tanmayrewale232021@gmail.com>
2025-02-03 19:54:09 +0530

    added subscription model

---
+++ 4-project/src/models/subscription.model.js
@@ -0,0 +1,14 @@
+import mongoose, {Schema} from 'mongoose'
+
+const subscriptionSchema = new Schema({
+  subscriber: {
+    type: Schema.Types.ObjectId,    // one who is subscribing
+    ref: "User"
+  },
+  channel: {
+    type: Schema.Types.ObjectId,    // one to whom subcriber is subscribing
+    ref: "User"
+  }
+}, {timestamps: true})
+
+export const Subscription = mmongoose.model("Subscription", subscriptionSchema)
